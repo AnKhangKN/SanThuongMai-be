08a8f5e31e550b8bc5a0a6aae5dff1e2925b743b
Phan An Khang <minecraftcopyright1302@gmail.com>
2025-03-17 20:13:06 +0700

    lll

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const routes = require('./routes');
+const mongoose = require('mongoose');
 
 dotenv.config();  
 
@@ -8,6 +9,14 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 routes(app);
+
+mongoose.connect(`${process.env.MONGO_DB}`)
+.then(() => {
+    console.log("Connect DB success!");
+})
+.catch((err) => {
+    console.log(err);
+});
 
 app.listen(port, () => {
     console.log("Server is running on port: " + port);
---
+++ src/models/Products.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    images: [
+        {
+            type: String, // Lưu URL ảnh hoặc đường dẫn ảnh
+            required: true,
+        }
+    ],
+    category: {
+        type: String,
+        required: true,
+    },
+
+
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
