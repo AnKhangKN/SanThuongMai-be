ebcf61b11aa4f096dec96e2e9c23bdcaae026280
Hieu Pham <hieu02617@gmail.com>
2025-05-14 00:05:19 +0700

    aaaa

--- src/controllers/Admin/ProductControllers.js
+++ src/controllers/Admin/ProductControllers.js
@@ -1,59 +1,60 @@
 const ProductServices = require("../../services/Admin/ProductServices");
 
 const getAllProducts = async (req, res) => {
-    try {
-        const result = await ProductServices.getAllProducts();
+  try {
+    const result = await ProductServices.getAllProducts();
 
-
-        return res.status(200).json(result);
-
-
-    } catch (error) {
-        return res.status(500).json({
-            message: error.message || "Internal Server Error",
-        });
-    }
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || "Internal Server Error",
+    });
+  }
 };
 
 const partialUpdateProduct = async (req, res) => {
-    try {
-        const productId = req.params.id;
-        const data = req.body;
+  try {
+    const productId = req.params.id;
+    const data = req.body;
 
-        // Kiểm tra nếu productId không tồn tại
-        if (!productId) {
-            return res.status(400).json({
-                status: "ERROR",
-                message: "Không có mã sản phẩm",
-            });
-        }
-
-        // Gọi service để cập nhật dữ liệu
-        const result = await ProductServices.partialUpdateProduct(productId, data);
-
-        return res.status(200).json({
-            status: "OK",
-            message: "Cập nhật sản phẩm thành công",
-            data: result,
-        });
-    } catch (err) {
-        return res.status(500).json({
-            status: "ERROR",
-            message: err.message || "Internal Server Error",
-        });
+    // Kiểm tra nếu productId không tồn tại
+    if (!productId) {
+      return res.status(400).json({
+        status: "ERROR",
+        message: "Không có mã sản phẩm",
+      });
     }
+
+    // Gọi service để cập nhật dữ liệu
+    const result = await ProductServices.partialUpdateProduct(productId, data);
+
+    return res.status(200).json({
+      status: "OK",
+      message: "Cập nhật sản phẩm thành công",
+      data: result,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: "ERROR",
+      message: err.message || "Internal Server Error",
+    });
+  }
 };
 
 const getAllReportedProducts = async (req, res) => {
-    try {
-        const result = await ProductServices.getAllReportedProducts();
+  try {
+    const result = await ProductServices.getAllReportedProducts();
 
-        return res.status(200).json(result);  // Trả về kết quả danh sách cửa hàng
-    } catch (error) {
-        return res.status(500).json({
-            message: error.message || "Internal Server Error",
-        });
-    }
+    return res.status(200).json(result); // Trả về kết quả danh sách cửa hàng
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || "Internal Server Error",
+    });
+  }
 };
 
-module.exports = { getAllProducts,partialUpdateProduct, getAllReportedProducts };
+module.exports = {
+  getAllProducts,
+  partialUpdateProduct,
+  getAllReportedProducts,
+};
